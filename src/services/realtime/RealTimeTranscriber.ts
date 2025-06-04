61acf269ff4271bfd0bb1a489cd1aeae55cfbae4
JessePires <jessepires2010@gmail.com>
2025-06-03 21:02:49 -0300

    fix(call): cleaning project

--- src/services/realtime/RealTimeTranscriber.ts
+++
@@ -1,6 +0,0 @@
-export interface RealtimeTranscriber {
-  connect(): Promise<void>;
-  sendAudioChunk(chunk: Buffer): void;
-  close(): void;
-  onTranscription(callback: (text: string, isFinal: boolean) => void): void;
-}
